53c14d622e798e92009dfc8c72e2e06402babce2
Uladzimir Barysenka <vovaborisenko@live.com>
2025-01-07 18:36:03 +0300

    adds controllers

---
+++ src/top-page/top-page.controller.ts
@@ -0,0 +1,32 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { TopPageModel } from './top-page.model';
+import { FindTopPageDto } from './dto/find-top-page.dto';
+
+@Controller('top-page')
+export class TopPageController {
+  @Post('create')
+  async create(@Body() dto: Omit<TopPageController, '_id'>) {}
+
+  @Get(':id')
+  async get(@Param('id') id: string) {}
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {}
+
+  @Patch(':id')
+  async patch(@Param('id') id: string, @Body() dto: TopPageModel) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post()
+  async find(@Body() dto: FindTopPageDto) {}
+}
